d5380fc5e08a1a92b6cf80ccd4d66331c38d9c88
Niamh Cassidy <2564109c@student.gla.ac.uk>
2023-05-24 15:19:26 +0100

    mocks

---
+++ tests/test.py
@@ -0,0 +1,17 @@
+def __init__(self):
+    self.print_board = None
+
+
+def print_board(self, board):
+    self.print_board = board
+
+
+def __init__(self, input_v):
+    self.input_v = input_v
+    self.input_index += 1
+
+
+def get_input(self, mess):
+    value = self.input_v[self.input_index]
+    self.input_index += 1
+    return value
